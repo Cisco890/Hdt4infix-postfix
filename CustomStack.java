154cae943f988f5d9a520d3d74488cc28699084d
Juan Francisco Martínez <jfmartinez890@gmail.com>
2024-02-19 20:26:53 -0600

    CustomStack y Nodos Genéricos

---
+++ CustomStack.java
@@ -0,0 +1,56 @@
+/** Anthony Lou Shwank 23410/Juan Francisco Martínez 23617
+
+Custom Stack Genérico
+
+@param   no_hay
+@throws Stacks Clase que crea la estructura y metodos que usan los stacks con datos genericos
+*/
+public class CustomStack<T> {
+    public Node<T> lastNode;
+    public Node<T> firstNode;
+
+
+      public void push(T value) {
+          if (lastNode == null) {
+              lastNode = new Node<T>(value);
+              firstNode = lastNode;
+          } else {
+              Node<T> current = new Node<T>(value);
+              current.setNext(firstNode);
+              firstNode = current;
+          }
+      }
+
+      public boolean isEmpty() {
+          return firstNode == null;
+      }
+
+      public T pop() {
+          if (firstNode != null) {
+              T value = firstNode.getValue();
+              firstNode = firstNode.getNext();
+              return value;
+          }
+          return null;
+      }
+
+      public int size() {
+          int count = 0;
+          Node<T> current = firstNode;
+          while (current != null) {
+              count++;
+              current = current.getNext();
+          }
+          return count;
+      }
+
+      public T[] toArray(T[] array) {
+          Node<T> current = firstNode;
+          int index = 0;
+          while (current != null && index < array.length) {
+              array[index++] = current.getValue();
+              current = current.getNext();
+          }
+          return array;
+      }
+    }
