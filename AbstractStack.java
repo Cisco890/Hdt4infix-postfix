0618ff91431058a588b24b613ab18f24aec06960
Anthony Lou <anthonylouschwank@gmail.com>
2024-02-26 19:14:57 -0600

    Removiendo codigo redundante

--- AbstractStack.java
+++ AbstractStack.java
@@ -1,6 +1,1 @@
-abstract class AbstractStack<T> {
-    public abstract void push(T element);
-    public abstract T pop();
-    public abstract T peek();
-    public abstract boolean isEmpty();
-}
+
